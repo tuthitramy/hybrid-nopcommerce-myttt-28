3915a2d1a51f1c5803ca537bfaac03358cd7555c
tuthitramy <96281101+tuthitramy@users.noreply.github.com>
2023-06-21 19:27:21 +0700

    Delete src directory

--- src/main/java/pageUIs/nopCommerce/user/WishlistPageUI.java
+++
@@ -1,8 +0,0 @@
-package pageUIs.nopCommerce.user;
-
-public class WishlistPageUI {
-	public static final String PRODUCT_NAME = "xpath=//td[@class='product-picture']/following-sibling::td[@class='product']/a";
-	public static final String ADD_TO_CART_CHECKBOX = "xpath=//tr//td[@class='add-to-cart']//input[@type='checkbox']";
-	public static final String REMOVE_PRODUCT_BUTTON = "xpath=//td[@class='remove-from-cart']//button[@class='remove-btn']";
-
-}
